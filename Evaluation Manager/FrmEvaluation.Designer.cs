4f16eeb1bd33a5505a092c5d2a6bc74981234cb7
evalec21 <evalec21@foi.hr>
2023-05-15 19:54:05 +0200

    Dodan TecherRepository

--- Evaluation Manager/FrmEvaluation.Designer.cs
+++ Evaluation Manager/FrmEvaluation.Designer.cs
@@ -176,7 +176,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(507, 450);
+            this.ClientSize = new System.Drawing.Size(498, 326);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.lbl_Bodovi);
